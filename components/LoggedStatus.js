902968119f1a30341855acee68503296fc9eb047
ZarNizza <gormaster@mail.ru>
2022-01-27 16:34:57 +0400

    UserName instead of Email

--- components/LoggedStatus.js
+++ components/LoggedStatus.js
@@ -3,9 +3,10 @@ import styles from "../styles/Home.module.css";
 
 export default function LoggedStatus() {
   const { data: session } = useSession();
+  console.log("---------- GOO session ------------", session);
   return session ? (
     <>
-      Signed in as <b>{session.user.email}</b>
+      Signed in as <b>{session.user.name}</b>
       <button onClick={() => signOut()} className={styles.logoutButton}>
         {" "}
         Sign out{" "}
