6159aad4c34df3f737f9dfedc948196ec22b1955
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2022-01-31 17:53:17 +0100

    add prettier settings

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,57 +1,57 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
-import { RadioButton } from "../components/RadioButton";
-import { MouseEventHandler, useEffect } from "react";
-import { useState } from "react";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Layout from '../components/layout'
+import styles from '../styles/Home.module.css'
+import { RadioButton } from '../components/RadioButton'
+import { MouseEventHandler, useEffect } from 'react'
+import { useState } from 'react'
 
 interface ProductItem {
-  pid: number;
-  psum: number;
+  pid: number
+  psum: number
 }
 
 const Home: NextPage = () => {
   const prod = [
-    { pid: 1, pname: "Маникюр", psymbol: "М" },
-    { pid: 2, pname: "Маникюр +Гель", psymbol: "М+Г" },
-    { pid: 3, pname: "Педикюр", psymbol: "П" },
-    { pid: 4, pname: "Педикюр +Гель", psymbol: "П+Г" },
-    { pid: 5, pname: "Подология", psymbol: "Подолог" },
-    { pid: 6, pname: "Бровки", psymbol: "Брови" },
-    { pid: 7, pname: "Реснички", psymbol: "ЛамРес" },
-  ];
+    { pid: 1, pname: 'Маникюр', psymbol: 'М' },
+    { pid: 2, pname: 'Маникюр +Гель', psymbol: 'М+Г' },
+    { pid: 3, pname: 'Педикюр', psymbol: 'П' },
+    { pid: 4, pname: 'Педикюр +Гель', psymbol: 'П+Г' },
+    { pid: 5, pname: 'Подология', psymbol: 'Подолог' },
+    { pid: 6, pname: 'Бровки', psymbol: 'Брови' },
+    { pid: 7, pname: 'Реснички', psymbol: 'ЛамРес' }
+  ]
   const [productList, setProductList] = useState<ProductItem[]>([
-    { pid: 0, psum: 0 },
-  ]);
+    { pid: 0, psum: 0 }
+  ])
 
   function radioHandler(e: any) {
-    const name = e.target.name;
-    const value = e.target.value * 1;
-    const checked = e.target.checked;
+    const name = e.target.name
+    const value = e.target.value * 1
+    const checked = e.target.checked
 
     console.log(
-      "--------- checked",
+      '--------- checked',
       checked,
       name,
       value,
-      "(",
+      '(',
       typeof value,
-      ")"
+      ')'
       // productList
-    );
-    const pIndex: number = productList.findIndex((item) => item.pid === value);
-    const inList = pIndex === -1 ? false : true;
+    )
+    const pIndex: number = productList.findIndex((item) => item.pid === value)
+    const inList = pIndex === -1 ? false : true
     if (checked && !inList) {
-      const tmpList = productList.slice();
-      tmpList.push({ pid: value, psum: 0 });
-      setProductList(() => tmpList);
+      const tmpList = productList.slice()
+      tmpList.push({ pid: value, psum: 0 })
+      setProductList(() => tmpList)
     }
     if (!checked && inList) {
       // setProductList((prevState) => prevState.splice(pIndex, 1));
     }
 
-    return;
+    return
   }
 
   function ProdSet() {
@@ -63,23 +63,23 @@ const Home: NextPage = () => {
         value={item.pid}
         handler={radioHandler}
       />
-    ));
-    return <div className={styles.flexRowContainer}>{prodSet}</div>;
+    ))
+    return <div className={styles.flexRowContainer}>{prodSet}</div>
   }
 
   function ProductList() {
     const qqq = productList.map((item) => (
       <li key={item.pid}>
-        {" "}
-        pid= {item.pid}, psum = {item.psum}{" "}
+        {' '}
+        pid= {item.pid}, psum = {item.psum}{' '}
       </li>
-    ));
+    ))
     return (
       <div>
         <p>ProductList</p>
         <ul>{qqq}</ul>
       </div>
-    );
+    )
   }
   //
   //
@@ -100,7 +100,7 @@ const Home: NextPage = () => {
         </div>
       </main>
     </Layout>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
