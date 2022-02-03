885abbda22c605223ce3e120f7d378ebaea8d85a
ZarNizza <gormaster@mail.ru>
2022-02-03 17:50:06 +0400

    fix types in ProductStore

--- components/ProductStore.tsx
+++ components/ProductStore.tsx
@@ -1,12 +1,15 @@
 import { CheckBoxButton } from './CheckBoxButton'
+import type { Product } from '../pages/add'
 import styles from '../styles/Home.module.css'
 
-export default function ProductStore(props) {
-  const prodSet = props.products.map((item) => {
+export default function ProductStore(props: any) {
+  const prodSet = props.products.map((item: Product) => {
     function checkHandler() {
-      props.setSelectedProducts((prevSelectedProducts) => {
+      props.setSelectedProducts((prevSelectedProducts: [Product['pid']]) => {
         return prevSelectedProducts.includes(item.pid)
-          ? prevSelectedProducts.filter((product) => product !== item.pid)
+          ? prevSelectedProducts.filter(
+              (product: Product['pid']) => product !== item.pid
+            )
           : [...prevSelectedProducts, item.pid]
       })
     }
