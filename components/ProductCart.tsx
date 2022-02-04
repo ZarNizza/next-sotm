5727dc0aac5a8a69a7bdd06de6d4d4bd94a33369
ZarNizza <gormaster@mail.ru>
2022-02-05 01:30:52 +0400

    tmp - refactor to Refs (not complete)

--- components/ProductCart.tsx
+++ components/ProductCart.tsx
@@ -1,9 +1,18 @@
 import type { Product } from '../pages/add'
 import styles from './ProductCart.module.scss'
-
 import Link from 'next/link'
+import { ChangeEventHandler, useEffect, useRef } from 'react'
 
 export default function ProductCart(props: any) {
+  const myRef = useRef<Record<Product['pid'], number>>({})
+  const grossSumRef = useRef<number>(0)
+
+  useEffect(() => {
+    const gS = document.getElementById('grossSum')
+    console.log('-------------- useEffect--------- gs=', gS)
+    if (gS !== null) gS.innerHTML = grossSumRef.current.toLocaleString('ru-RU')
+  }, [grossSumRef.current])
+
   function dropHandler(e: any) {
     props.setSelectedProducts((prevSelectedProducts: [Product['pid']]) => {
       return prevSelectedProducts.filter(
@@ -12,11 +21,42 @@ export default function ProductCart(props: any) {
     })
   }
 
+  function dropHandler2(pid: Product['pid']) {
+    return () => {
+      props.setSelectedProducts((prevSelectedProducts: [Product['pid']]) => {
+        delete myRef.current[pid]
+        return prevSelectedProducts.filter((product) => product !== Number(pid))
+      })
+    }
+  }
+
   function inputChange() {
-    const qList: any[] = Array.from(document.getElementsByName('pSum'))
+    const qList = Array.from(
+      document.getElementsByName('pSum')
+    ) as HTMLInputElement[]
     const gSum = qList.reduce((prev, curr) => prev + Number(curr.value), 0)
     const gS = document.getElementById('grossSum')
     if (gS !== null) gS.innerHTML = gSum.toLocaleString('ru-RU')
+  }
+  //
+  // - чистка myRef при снятии чека на чекбоксе
+  // - обновлять гроссСум
+  //
+  //
+  //
+
+  function inputChange2(pid: Product['pid']) {
+    const handler: ChangeEventHandler<HTMLInputElement> = (event) => {
+      console.log('for pid-', pid, ' the new value =', event.target.value)
+      myRef.current[pid] = Number(event.target.value)
+      console.log('myRefCurr[]=', myRef.current)
+      grossSumRef.current = Object.values(myRef.current).reduce(
+        (prev, curr) => prev + curr,
+        0
+      )
+      console.log('grossSum=', grossSumRef.current)
+    }
+    return handler
   }
 
   const qqq = props.selectedProducts.map((pid: Product['pid']) => (
@@ -24,7 +64,7 @@ export default function ProductCart(props: any) {
       <input
         type="text"
         name="pSum"
-        onChange={inputChange}
+        onChange={inputChange2(pid)}
         className={styles.inputSum}
         placeholder="price"
       />{' '}
@@ -35,7 +75,11 @@ export default function ProductCart(props: any) {
           }
         ).pname
       }{' '}
-      <button value={pid} onClick={dropHandler} className={styles.dropButton}>
+      <button
+        value={pid}
+        onClick={dropHandler2(pid)}
+        className={styles.dropButton}
+      >
         {' '}
         X{' '}
       </button>
@@ -43,12 +87,13 @@ export default function ProductCart(props: any) {
   ))
 
   function setHandler() {
-    const qList: any[] = Array.from(document.getElementsByName('pSum'))
-    props.selectedProducts.map((item: Product, i: number) => {
+    // const qList: any[] = Array.from(document.getElementsByName('pSum'))
+    props.selectedProducts.map((pid: number) => {
       const sale = {
         customer: props.currentCustomer[0],
-        prod: item,
-        sum: qList[i].value
+        prod: pid,
+        // sum: qList[i].value
+        sum: myRef.current[pid]
       }
       console.log('sale=', sale)
       fetch('/api/sales', {
@@ -92,8 +137,9 @@ export default function ProductCart(props: any) {
             <span className={styles.grossSum} id="grossSum"></span>
             <button onClick={setHandler} className={styles.buttonOk}>
               {' '}
-              Sale it!{' '}
+              Sale it!
             </button>
+            {grossSumRef.current}
           </div>
         </>
       )}
