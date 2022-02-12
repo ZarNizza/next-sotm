2486b06ee3867a661c78159e48e6eed259a39064
ZarNizza <gormaster@mail.ru>
2022-02-12 08:32:59 +0400

    (sys) fill X

---
+++ pages/api/sys_fill_X.ts
@@ -0,0 +1,76 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mysql from 'mysql2'
+import type { Sale, Customer, Product } from '../add'
+import type { Xpense } from '../expenses'
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
+
+function SaveXpense(args: Xpense) {
+  return new Promise((resolveSS, rejectSS) => {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err // not connected!
+      connection.query(
+        'INSERT INTO xpenses (xdate, xitem, xsum) VALUES (?, ?, ?)',
+        [args.xdate, Number(args.xitem), Number(args.xsum)],
+        function (error, results, fields) {
+          connection.release()
+          if (error) {
+            rejectSS(error)
+          } else {
+            resolveSS(null)
+            return
+          }
+        }
+      )
+    })
+    resolveSS(null)
+  })
+}
+
+export default function sysHandler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('NEW FILL XPENSES')
+  const expItems = [1, 2, 3]
+
+  let iDate = new Date(2022, 1, 10, 11)
+  const findate = new Date(2022, 1, 10, 11)
+  let dates: string[] = []
+
+  for (; iDate <= findate; iDate.setDate(iDate.getDate() + 1)) {
+    dates.push(serialiseDate(iDate))
+  }
+
+  return Promise.all(
+    expItems
+      .map((xitem) => {
+        return dates.map((xdate) => {
+          const xsum = xitem
+          return SaveXpense({ xdate, xitem, xsum })
+        })
+      })
+      .flat(1)
+  )
+    .then(() => res.status(203).json({ data: 'OK' }))
+    .catch((error) =>
+      res.status(500).json({ error: String('!api/sys fill_X err:' + error) })
+    )
+}
+
+const timeZone = '04'
+function serialiseDate(date: Date) {
+  return (
+    String(date.getFullYear()) +
+    '-' +
+    String(date.getMonth() + 1) +
+    '-' +
+    String(date.getDate()) +
+    'T' +
+    timeZone +
+    ':00:00'
+  )
+}
