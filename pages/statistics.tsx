f657fadeaa407815497facb56fdab98f68ea2e6e
ZarNizza <gormaster@mail.ru>
2022-02-13 14:34:22 +0400

    stat - rename CS-Full

--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -223,7 +223,7 @@ const Home: NextPage = () => {
             <button onClick={show_X_Handler}>Short X report</button>
             <button onClick={show_SX_Handler}>Short SX report</button>
             <button onClick={show_SX_Full_Handler}>SX Full report</button>
-            <button onClick={show_CS_Full_Handler}>Customers report</button>
+            <button onClick={show_CS_Full_Handler}>CS Full report</button>
           </div>
           {resData === undefined || resData.length === 0 ? (
             <p>No data - empty result</p>
