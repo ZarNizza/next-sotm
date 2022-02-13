dcb447edb304b8f65fb1331bc7220b28799d7581
ZarNizza <gormaster@mail.ru>
2022-02-13 19:18:39 +0400

    fix serialise

--- components/serialiseDate.tsx
+++ components/serialiseDate.tsx
@@ -2,9 +2,11 @@ export default function serialiseDate(date: Date, timeZone: string) {
   return (
     String(date.getFullYear()) +
     '-' +
-    String(date.getMonth() + 1) +
+    String(
+      date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+    ) +
     '-' +
-    String(date.getDate()) +
+    String(date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) +
     'T' +
     timeZone +
     ':00:00'
