c8f155b209bcfd8218c4889a4e7d33723be010d9
ZarNizza <gormaster@mail.ru>
2022-02-25 09:48:35 +0400

    settings page - MVP

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 
@@ -11,8 +12,39 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <h3>Settings page</h3>
-        <p>Page in the development queue.</p>
         <p>Admin / system settings.</p>
+        <div className={styles.row}>
+          <Link href="/customers" passHref>
+            <div className={styles.square_plus}>
+              <p> Customers </p>
+            </div>
+          </Link>
+
+          <Link href="/sys" passHref>
+            <div className={styles.square_sys}>
+              <p> System </p>
+            </div>
+          </Link>
+        </div>
+        <div className={styles.row}>
+          <Link href="/Sales" passHref>
+            <div className={styles.square_stat}>
+              <p> Sales </p>
+            </div>
+          </Link>
+          <div className={styles.square0}>
+            <Link href="/eitems" passHref>
+              <div className={styles.halfsquare_top}>
+                <p>Cost Items</p>
+              </div>
+            </Link>
+            <Link href="/expenses" passHref>
+              <div className={styles.halfsquare_bottom}>
+                <p> Expenses </p>
+              </div>
+            </Link>
+          </div>
+        </div>
       </main>
     </Layout>
   )
