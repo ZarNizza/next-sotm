7b55dcad18f4fd510a7da4f41156657ff192c6cc
ZarNizza <gormaster@mail.ru>
2022-03-16 15:08:46 +0400

    tmp

--- components/Footer.js
+++ components/Footer.js
@@ -8,69 +8,71 @@ export default function Footer() {
   // return session ? (
   return (
     <div className={styles.footer}>
-      <span>
-        <Link href="/" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_p}>
-              <div className={styles.footerLabel}>&#8962;</div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/plus" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_o}>
-              <div className={styles.footerLabel}> + </div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/minus" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_b}>
-              <div className={styles.footerLabel}> – </div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/memo" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_y}>
-              <div className={styles.footerLabel}>M</div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/statistics" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_g}>
-              <div className={styles.footerLabel}> = </div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/settings" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_pink}>
-              <div className={styles.footerLabel}>Set</div>
-            </span>
-          </a>
-        </Link>
-      </span>
-      <span>
-        <Link href="/sys" passHref>
-          <a className={styles.footerLink}>
-            <span className={styles.rb_p}>
-              <div className={styles.footerLabel}>[Sys]</div>
-            </span>
-          </a>
-        </Link>
-      </span>
+      <div className={styles.footerLine}>
+        <span>
+          <Link href="/" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_p}>
+                <div className={styles.footerLabel}>&#8962;</div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/plus" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_o}>
+                <div className={styles.footerLabel}> + </div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/minus" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_b}>
+                <div className={styles.footerLabel}> – </div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/memo" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_y}>
+                <div className={styles.footerLabel}>M</div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/statistics" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_g}>
+                <div className={styles.footerLabel}> = </div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/settings" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_pink}>
+                <div className={styles.footerLabel}>Set</div>
+              </span>
+            </a>
+          </Link>
+        </span>
+        <span>
+          <Link href="/sys" passHref>
+            <a className={styles.footerLink}>
+              <span className={styles.rb_p}>
+                <div className={styles.footerLabel}>[Sys]</div>
+              </span>
+            </a>
+          </Link>
+        </span>
+      </div>
     </div>
   )
 }
