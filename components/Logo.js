f984992680872563be3b34ee8ee5aa81ad9866a9
ZarNizza <gormaster@mail.ru>
2022-03-18 15:59:02 +0400

    logo as image

--- components/Logo.js
+++ components/Logo.js
@@ -1,45 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Logo() {
   return (
     <div className={styles.rowElem}>
       <Link href="/" passHref>
-        <div className={styles.logoBH}>
-          <span style={{ margin: 'auto', color: 'white', fontSize: 'larger' }}>
-            <p
-              style={{
-                textAlign: 'end',
-                margin: '0.5rem 0.5rem -0.2rem 0',
-                fontSize: 'small'
-              }}
-            >
-              be
-            </p>
-            Happy!
-            <p
-              style={{
-                marginLeft: '0.9rem',
-                marginTop: '-0.5rem',
-                fontSize: 'x-large'
-              }}
-            >
-              &#128504;
-              <span
-                style={{
-                  fontSize: 'x-small',
-                  marginLeft: '0.9rem',
-                  marginTop: '-0.95rem',
-                  display: 'block',
-                  position: 'relative',
-                  top: '-0.4rem'
-                }}
-              >
-                &#128504;
-              </span>
-            </p>
-          </span>
-        </div>
+        <div className={styles.logoBH}></div>
       </Link>
     </div>
   )
