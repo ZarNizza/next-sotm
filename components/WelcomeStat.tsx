0ecd5a56ffd6fce70369b55dfb81bbd8f9fdb162
ZarNizza <gormaster@mail.ru>
2022-03-27 21:39:29 +0400

    WelcomeStat fix

--- components/WelcomeStat.tsx
+++ components/WelcomeStat.tsx
@@ -1,7 +1,7 @@
 import type { apiBody } from '../pages/statistics'
 import myDate from './MyDate'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function WelcomeStat() {
   const [statNow, setStatNow] = useState<number>(0)
@@ -17,45 +17,48 @@ export default function WelcomeStat() {
     finishDate: myDate('F-M')
   }
 
-  fetch('/api/statistics', {
-    method: 'POST',
-    body: JSON.stringify(bodyNow)
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res.error) {
-        console.log('!!! API error=', res.error)
-        alert('!Error: ' + res.error)
-      } else {
-        setStatNow(() => {
-          return isFinite(res.data[0].sum) ? res.data[0].sum : 0
-        })
-      }
+  useEffect(() => {
+    fetch('/api/statistics', {
+      method: 'POST',
+      body: JSON.stringify(bodyNow)
     })
-    .catch((error) => {
-      console.log('!!! catch Error:', error.message)
-      alert('!catch Error:' + error.message)
-    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.log('!!! API error=', res.error)
+          alert('!Error: ' + res.error)
+        } else {
+          setStatNow(() => {
+            return isFinite(res.data[0].sum) ? res.data[0].sum : 0
+          })
+        }
+      })
+      .catch((error) => {
+        console.log('!!! catch Error:', error.message)
+        alert('!catch Error:' + error.message)
+      })
 
-  fetch('/api/statistics', {
-    method: 'POST',
-    body: JSON.stringify(bodyPrev)
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res.error) {
-        console.log('!!! API error=', res.error)
-        alert('!Error: ' + res.error)
-      } else {
-        setStatPrev(() => {
-          return isFinite(res.data[0].sum) ? Number(res.data[0].sum) : 0
-        })
-      }
+    fetch('/api/statistics', {
+      method: 'POST',
+      body: JSON.stringify(bodyPrev)
     })
-    .catch((error) => {
-      console.log('!!! catch Error:', error.message)
-      alert('!catch Error:' + error.message)
-    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          console.log('!!! API error=', res.error)
+          alert('!Error: ' + res.error)
+        } else {
+          setStatPrev(() => {
+            return isFinite(res.data[0].sum) ? Number(res.data[0].sum) : 0
+          })
+        }
+      })
+      .catch((error) => {
+        console.log('!!! catch Error:', error.message)
+        alert('!catch Error:' + error.message)
+      })
+  }, [])
+
   return (
     <div className={styles.statRow}>
       {statNow} / {statPrev}
